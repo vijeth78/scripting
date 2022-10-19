ed6642fef235fae5eaa706e9278c3d650129682a
viji <17941934+vijeth78@users.noreply.github.com>
2022-10-18 19:10:18 -0700

    Update tcl_prog.tcl

--- tcl_prog.tcl
+++ tcl_prog.tcl
@@ -9,3 +9,15 @@ set numbers "1 2 3 4 5"
 }
 
 puts [avg]
+
+#packages 
+package require Tcl 8.6.12
+package require math::linearalgebra 
+namespace import math::linearalgebra::*
+
+set A {{1 2 3} {4 5 6 } {7 8 9}}
+puts [transpose $A]
+puts [getcol $A 2]
+
+
+
