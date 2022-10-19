8c7fd2de3841860f603755ab5c8b92231823143a
viji <17941934+vijeth78@users.noreply.github.com>
2022-10-18 23:29:41 -0700

    Update tcl_prog.tcl

--- tcl_prog.tcl
+++ tcl_prog.tcl
@@ -19,5 +19,17 @@ set A {{1 2 3} {4 5 6 } {7 8 9}}
 puts [transpose $A]
 puts [getcol $A 2]
 
+package require struct::list
+puts [struct::list map {1 2 3 4} format %.6f}]
 
+#Dictionary
+dict set names joe 10  
+dict set names john 12
+puts $names
+#get item
+puts "joe=[dict get $names joe]"
+#Dict interation
+foreach item [dict keys $names] {
+puts "$item=[dict get $names $item]"
+}
 
